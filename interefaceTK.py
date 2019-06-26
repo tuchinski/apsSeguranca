dd3542e9603335883626118998e09f230197a449
rmmenezes <ra29fa@gmail.com>
2019-06-25 21:39:26 -0300

    tela de login e select user

--- interefaceTK.py
+++ interefaceTK.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import tkinter as tk
 from passwdNG import Psswd
 
@@ -124,91 +123,6 @@ class Login(tk.Frame):
         botao_login["width"] = 58
         botao_login.pack(pady=5)
   
-=======
-from tkinter import *
-from passwdNG import Psswd
-  
-class Application:
-    def __init__(self, master, passwdNG):
-        self.fontePadrao = ("Arial", "10")
-        master.title("PasswdNG")
-
-        ###### DEFINE CONTAINER 01 ######
-        self.primeiroContainer = Frame(master)
-        self.primeiroContainer["pady"] = 10
-        self.primeiroContainer.pack()
-  
-        ###### DEFINE CONTAINER 02 ######
-        self.segundoContainer = Frame(master)
-        self.segundoContainer["padx"] = 20
-        self.segundoContainer.pack()
-  
-        ###### DEFINE CONTAINER 03 ######
-        self.terceiroContainer = Frame(master)
-        self.terceiroContainer["padx"] = 20
-        self.terceiroContainer.pack()
-  
-        ###### DEFINE CONTAINER 04 ######
-        self.quartoContainer = Frame(master)
-        self.quartoContainer["pady"] = 20
-        self.quartoContainer.pack()
-
-        ###### DEFINE CONTAINER 05 ######
-        self.quintoContainer = Frame(master)
-        self.quintoContainer["padx"] = 20
-        self.quintoContainer.pack()
-    
-        self.init_window()
-    
-    def init_window(self):
-        self.titulo = Label(self.primeiroContainer, text="Dados do usuário")
-        self.titulo["font"] = ("Arial", "10", "bold")
-        self.titulo.pack()
-  
-        self.nomeLabel = Label(self.segundoContainer,text="Nome", font=self.fontePadrao)
-        self.nomeLabel.pack(side=LEFT)
-  
-        self.nome = Entry(self.segundoContainer)
-        self.nome["width"] = 30
-        self.nome["font"] = self.fontePadrao
-        self.nome.pack(side=LEFT)
-  
-        self.senhaLabel = Label(self.terceiroContainer, text="Senha", font=self.fontePadrao)
-        self.senhaLabel.pack(side=LEFT)
-  
-        self.senha = Entry(self.terceiroContainer)
-        self.senha["width"] = 30
-        self.senha["font"] = self.fontePadrao
-        self.senha["show"] = "*"
-        self.senha.pack(side=LEFT)
-  
-        self.autenticar = Button(self.quartoContainer)
-        self.autenticar["text"] = "Autenticar"
-        self.autenticar["font"] = ("Calibri", "8")
-        self.autenticar["width"] = 12
-        self.autenticar["command"] = self.verificaSenha
-        self.autenticar.pack()
-  
-        self.mensagem = Label(self.quartoContainer, text="", font=self.fontePadrao)
-        self.mensagem.pack()
-
-        self.lbl = Label(self.quintoContainer)  
-        self.lbl["text"] = "List of Users"
-        self.lbl["font"] = ("Calibri", "8")
-
-        list_users = passwdNG.get_file_shadow()
-        tonkes = passwdNG.get_tokens_by_user_shadow()
-       
-        i = 1
-        self.listbox = Listbox(self.quintoContainer)
-        self.listbox["width"] = 100
-        for line in tonkes:
-            self.listbox.insert(i,line[0])
-            i = i + 1
-
-        self.lbl.pack()  
-        self.listbox.pack()
->>>>>>> 079374f3af4eeb6be57286addd58345d8c35371f
   
     #Método verificar senha
     def verificaSenha(self):
@@ -220,7 +134,6 @@ class Application:
             self.mensagem["text"] = "Erro na autenticação"
   
   
-<<<<<<< HEAD
 if __name__ == "__main__":
     
     backend_passwd = Psswd()
@@ -228,9 +141,3 @@ if __name__ == "__main__":
     app.title("NEW PASSWD")
     app.geometry("500x200")
     app.mainloop()
-=======
-root = Tk()
-passwdNG = Psswd()
-Application(root, passwdNG)
-root.mainloop()
->>>>>>> 079374f3af4eeb6be57286addd58345d8c35371f
