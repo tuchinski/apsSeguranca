3942cdcf6119f5b181984cf2c962587d4d73b2b3
rmmenezes <ra29fa@gmail.com>
2019-06-27 10:12:27 -0300

    valid password tkinter

--- interefaceTK.py
+++ interefaceTK.py
@@ -1,5 +1,7 @@
 import tkinter as tk
+import re
 from passwdNG import Psswd
+import time
 
 
 class SampleApp(tk.Tk):
@@ -87,6 +89,10 @@ class Login(tk.Frame):
         terceiroContainer = tk.Frame(self)
         terceiroContainer.pack()
 
+        ###### DEFINE CONTAINER 04 ######
+        quartoContainer = tk.Frame(self)
+        quartoContainer.pack()
+
         self.titulo = tk.Label(titleContainer, text="-- Login --")
         self.titulo["font"] = ("Arial", "15", "bold")
         self.titulo.pack(pady=5)
@@ -96,33 +102,37 @@ class Login(tk.Frame):
         self.nomeLabel["justify"] = tk.LEFT
         self.nomeLabel.pack(side=tk.LEFT, pady=5)
 
-        self.nome = tk.Entry(primeiroContainer)
-        self.nome["width"] = 50
-        self.nome.pack(side=tk.RIGHT, pady=5)
+        self.username = tk.Entry(primeiroContainer)
+        self.username["width"] = 50
+        self.username.pack(side=tk.RIGHT, pady=5)
 
         self.senhaLabel = tk.Label(segundoContainer, text="Password:", anchor="w")
         self.senhaLabel["width"] = 10
         self.senhaLabel["justify"] = tk.LEFT
         self.senhaLabel.pack(side=tk.LEFT, pady=5)
 
-        self.senha = tk.Entry(segundoContainer, show="*")
-        self.senha["width"] = 50
-        self.senha.pack(side=tk.RIGHT, pady=5)
+        self.password = tk.Entry(segundoContainer, show="*")
+        self.password["width"] = 50
+        self.password.pack(side=tk.RIGHT, pady=5)
 
-        self.botao_login = tk.Button(terceiroContainer, command=lambda: master.switch_frame(Painel, passwdNG))
+        self.botao_login = tk.Button(terceiroContainer, command=lambda: self.get_data_login_user(str(self.username.get()), str(self.password.get())))
         self.botao_login["text"] = "Login"
         self.botao_login["width"] = 58
         self.botao_login.pack(pady=5)
-  
-  
-    #Método verificar senha
-    def verificaSenha(self):
-        usuario = self.nome.get()
-        senha = self.senha.get()
-        if usuario == "usuariodevmedia" and senha == "dev":
-            self.mensagem["text"] = "Autenticado"
+
+        self.lb_alert = tk.Label(quartoContainer)
+        self.lb_alert["text"] = ""
+        self.lb_alert["font"] = ("Arial", "9", "bold")
+        self.lb_alert.pack()
+
+    def get_data_login_user(self, username, password):
+        if ((username and password) != ""):
+            self.lb_alert["text"] =  "Sucessfull Login."
+            self.lb_alert["fg"] = "green"
+            self.master.switch_frame(Painel, self.passwdNG)
         else:
-            self.mensagem["text"] = "Erro na autenticação"
+            self.lb_alert["text"] =  "Please fill in all fields."
+            self.lb_alert["fg"] = "red"
   
 
 class Painel(tk.Frame):
@@ -209,24 +219,61 @@ class EditPass(tk.Frame):
         self.pass_new2["width"] = 30
         self.pass_new2.pack(side=tk.RIGHT, pady=5)
 
-        self.botao_Finish = tk.Button(quartoContainer, command=lambda: master.switch_frame(SelectUser, passwdNG))
+        self.botao_Finish = tk.Button(quartoContainer, command=lambda: self.get_data_edit_user(str(self.pass_current.get()), str(self.pass_new1.get()), str(self.pass_new2.get())))
         self.botao_Finish["text"] = "Finish"
         self.botao_Finish["width"] = 58
         self.botao_Finish.pack(pady=5)
 
+        self.lb_alert = tk.Label(quartoContainer)
+        self.lb_alert["text"] = ""
+        self.lb_alert["font"] = ("Arial", "9", "bold")
+        self.lb_alert.pack()
+
+
     def get_data_edit_user(self, username, pass1, pass2):
         if ((username and pass1 and pass2) != ""):
             if pass1 == pass2:
-                self.titulo["text"] =  "The user was created."
-                self.titulo["fg"] = "green"
-                print(self.passwdNG.get_file_shadow())
+                if (self.valid_password(pass1) == True):
+                    self.lb_alert["text"] =  "The user was created."
+                    self.lb_alert["fg"] = "green"
+                    time.sleep(0.3)
+                    self.master.switch_frame(SelectUser, self.passwdNG)
             else:
-                self.titulo["text"] =  "The passwords do not match."
-                self.titulo["fg"] = "red"
+                self.lb_alert["text"] =  "The passwords do not match."
+                self.lb_alert["fg"] = "red"
         else:
-            self.titulo["text"] =  "Please fill in all fields."
-            self.titulo["fg"] = "red"
+            self.lb_alert["text"] =  "Please fill in all fields."
+            self.lb_alert["fg"] = "red"
 
+    def valid_password(self, password):
+        digit = re.search(r"\d", password)
+        uppercase = re.search(r"[A-Z]", password)
+        lowercase = re.search(r"[a-z]", password) 
+        symbol = re.search(r"[ !#$%@%&'()*+,-./[\\\]^_`{|}~"+r'"]', password) 
+        
+        print (str(len(str(password))), digit, uppercase, lowercase, symbol)
+        print(len(str(password)))
+        if len(str(password)) < 6:
+            self.lb_alert["text"] =  "Passwords should consist of 6 to 8 characters."
+            self.lb_alert["fg"] = "red"
+        if digit == None:
+            self.lb_alert["text"] =  "Passwords should contain digits [0-9]."
+            self.lb_alert["fg"] = "red"
+        if uppercase == None:
+            self.lb_alert["text"] =  "Passwords should contain upper case characters."
+            self.lb_alert["fg"] = "red"
+        if lowercase == None:
+            self.lb_alert["text"] =  "Passwords should contain lower case characters."
+            self.lb_alert["fg"] = "red"
+        if symbol == None:
+            self.lb_alert["text"] =  "Passwords should contain symbols."
+            self.lb_alert["fg"] = "red"
+        
+        if (digit != None and uppercase != None and lowercase != None and symbol != None):
+            self.lb_alert["text"] =  ""
+            return (True)
+        else:
+            return (False)
 
 class CreateUser(tk.Frame):
     def __init__(self, master, passwdNG, data=None):
@@ -289,23 +336,23 @@ class CreateUser(tk.Frame):
         self.botao_Finish["width"] = 58
         self.botao_Finish.pack(pady=5)
     
-        self.titulo = tk.Label(quartoContainer)
-        self.titulo["text"] = ""
-        self.titulo["font"] = ("Arial", "9", "bold")
-        self.titulo.pack()
+        self.lb_alert = tk.Label(quartoContainer)
+        self.lb_alert["text"] = ""
+        self.lb_alert["font"] = ("Arial", "9", "bold")
+        self.lb_alert.pack()
 
     def get_data_new_user(self, username, pass1, pass2):
         if ((username and pass1 and pass2) != ""):
             if pass1 == pass2:
-                self.titulo["text"] =  "The user was created."
-                self.titulo["fg"] = "green"
+                self.lb_alert["text"] =  "The user was created."
+                self.lb_alert["fg"] = "green"
                 print(self.passwdNG.get_file_shadow())
             else:
-                self.titulo["text"] =  "The passwords do not match."
-                self.titulo["fg"] = "red"
+                self.lb_alert["text"] =  "The passwords do not match."
+                self.lb_alert["fg"] = "red"
         else:
-            self.titulo["text"] =  "Please fill in all fields."
-            self.titulo["fg"] = "red"
+            self.lb_alert["text"] =  "Please fill in all fields."
+            self.lb_alert["fg"] = "red"
 
   
 if __name__ == "__main__":
