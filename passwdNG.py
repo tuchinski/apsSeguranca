e903e36666bccf1cf827638cf6654b885b8267f6
Allison Sampaio <allisonsampaiox@gmail.com>
2019-07-16 00:18:12 -0300

    Arrumei a função de recuperação de senha por email

--- passwdNG.py
+++ passwdNG.py
@@ -342,37 +342,37 @@ class Psswd:
         return self.rules_pass
 
     def recuperarSenha(email):
-    alfabeto = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    newPassword = "".join(random.sample(alfabeto,8))
+        alfabeto = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        newPassword = "".join(random.sample(alfabeto,8))
 
-    redefinirSenha(encontrarUsuarioPorEmail(email), newPassword)
+        redefinirSenha(encontrarUsuarioPorEmail(email), newPassword)
 
-    msg = MIMEMultipart() # Cria instância do objeto de mensagem
-    message = " Olá! Sua nova senha para o PasswdNG é: " + newPassword + ".\n Recomendamos que após o login você altere a senha."
-     
-    # Parâmetros da mensagem
-    password = "seguranca"
-    msg['From'] = "pwdng2019@gmail.com"
-    msg['To'] = email
-    msg['Subject'] = "Sua nova senha do PasswdNG"
-     
-    # Adiciona mensagem ao corpo do e-mail
-    msg.attach(MIMEText(message, 'plain'))
+        msg = MIMEMultipart() # Cria instância do objeto de mensagem
+        message = " Olá! Sua nova senha para o PasswdNG é: " + newPassword + ".\n Recomendamos que após o login você altere a senha."
+         
+        # Parâmetros da mensagem
+        password = "seguranca"
+        msg['From'] = "pwdng2019@gmail.com"
+        msg['To'] = email
+        msg['Subject'] = "Sua nova senha do PasswdNG"
+         
+        # Adiciona mensagem ao corpo do e-mail
+        msg.attach(MIMEText(message, 'plain'))
 
-    # Cria o server e conecta
-    server = smtplib.SMTP('smtp.gmail.com: 587')
-    server.starttls()
-     
-    # Credenciais de Login
-    server.login(msg['From'], password)
-     
-    # Envia mensagem ao servidor
-    server.sendmail(msg['From'], msg['To'], msg.as_string())
-     
-    # Fecha conexão com o servidor
-    server.quit()
+        # Cria o server e conecta
+        server = smtplib.SMTP('smtp.gmail.com: 587')
+        server.starttls()
+         
+        # Credenciais de Login
+        server.login(msg['From'], password)
+         
+        # Envia mensagem ao servidor
+        server.sendmail(msg['From'], msg['To'], msg.as_string())
+         
+        # Fecha conexão com o servidor
+        server.quit()
 
-    print "E-mail enviado com sucesso para %s:" % (msg['To'])
+        print "E-mail enviado com sucesso para %s:" % (msg['To'])
 
     def encontrarUsuarioPorEmail(email):
         file = open('/etc/passwd')
