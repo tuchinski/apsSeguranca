aec1aa46b1bd54d39ecd6de8bc8e3f975127edcd
Allison Sampaio <allisonsampaiox@gmail.com>
2019-07-16 00:06:45 -0300

    Feito a recuperação de senha por email

--- passwdNG.py
+++ passwdNG.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import crypt
 import string
 import random
@@ -5,7 +6,11 @@ from random import choice
 import datetime
 from datetime import date
 import os
-
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+import fileinput
+import sys
 
 class Psswd:
     def __init__(self):
@@ -335,6 +340,73 @@ class Psswd:
 
     def get_value_pass(self):
         return self.rules_pass
+
+    def recuperarSenha(email):
+    alfabeto = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    newPassword = "".join(random.sample(alfabeto,8))
+
+    redefinirSenha(encontrarUsuarioPorEmail(email), newPassword)
+
+    msg = MIMEMultipart() # Cria instância do objeto de mensagem
+    message = " Olá! Sua nova senha para o PasswdNG é: " + newPassword + ".\n Recomendamos que após o login você altere a senha."
+     
+    # Parâmetros da mensagem
+    password = "seguranca"
+    msg['From'] = "pwdng2019@gmail.com"
+    msg['To'] = email
+    msg['Subject'] = "Sua nova senha do PasswdNG"
+     
+    # Adiciona mensagem ao corpo do e-mail
+    msg.attach(MIMEText(message, 'plain'))
+
+    # Cria o server e conecta
+    server = smtplib.SMTP('smtp.gmail.com: 587')
+    server.starttls()
+     
+    # Credenciais de Login
+    server.login(msg['From'], password)
+     
+    # Envia mensagem ao servidor
+    server.sendmail(msg['From'], msg['To'], msg.as_string())
+     
+    # Fecha conexão com o servidor
+    server.quit()
+
+    print "E-mail enviado com sucesso para %s:" % (msg['To'])
+
+    def encontrarUsuarioPorEmail(email):
+        file = open('/etc/passwd')
+        for i in file.readlines():
+            if email in i:
+                line = i
+        usuario = line.split(':')
+        usuario = usuario[0]
+        return usuario
+
+    def redefinirSenha(usuario, senha):
+        newPassword = encriptPassword(senha)
+
+        file = open('/etc/passwd')
+        for i in file.readlines():
+            if usuario in i:
+                oldPassword = (i[len(usuario)+1 : i.index(':', i.index(':') + 1)])
+                i.replace(oldPassword, newPassword)
+
+                for line in fileinput.FileInput("/etc/shadow",inplace=1):
+                   line = line.replace(oldPassword,newPassword)
+                   sys.stdout.write(line)
+
+    def encriptPassword(senha):
+        salt = '$6$'
+        possibleSalt = string.ascii_letters + string.digits
+
+        for a in range(8):
+            salt = salt + choice(possibleSalt)
+
+        salt = salt + '$'
+        encriptedPassword = crypt.crypt(senha,salt)
+
+        return encriptedPassword
 
 
 if __name__ == "__main__":
